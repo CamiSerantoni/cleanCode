40b41a2dfa43e27cc830ec822dc7c1a63285e0be
Camila Serantoni <camila.serantoni@blue.cl>
2024-10-08 08:45:15 -0300

    aplicando DRY al codigo

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -15,25 +15,54 @@ class Product {
 
 
     constructor(
-        public name: string ='',
+        public name: string = '',
         public price: number = 0,
-        public size: Size = '' ,
-    ){}
+        public size: Size = '',
+
+    ) { }
+
+    isProductReady(): boolean {
+        for (const key in this) {
+            switch (typeof this[key]) {
+                case 'string':
+                    if ((<string><unknown>this[key]).length <= 0) throw Error(`${key} is empty`);
+                    break;
+                case 'number':
+                    if ((<number><unknown>this[key]) <= 0) throw Error(`${key} is Zero`);
+
+                    break
+                default:
+                    throw Error(`${typeof this[key]} is not supported`);
+            }
 
 
-    toString(){
+
+        } return true;
+
+
+    }
+    /* <string><unknown>  a esto se le llama casteonismo de tipado 
+        
+        lo que quiere decir le decirmos "tratalo como si fuera... "
+        para que funcione con ts y no nos de error
+        */
+
+    toString() {
+
+
         // NO SE APLICA dry en este caso si cada vez que se agrega
         // una nueva propiedad se debe duplicar el codigo 
-        if(this.name.length <= 0) throw new Error('Name is empty'); 
-        if(this.price <= 0) throw new Error('price is 0'); 
-        if(this.size.length <= 0) throw new Error('size is empty');            
-        
+        // if(this.name.length <= 0) throw new Error('Name is empty'); 
+        // if(this.price <= 0) throw new Error('price is 0'); 
+        // if(this.size.length <= 0) throw new Error('size is empty');            
+
+        if (!this.isProductReady) return
 
         return `${this.name} (${this.price}, ${this.size})`;
     }
 }
 
-(()=> {
-    const bluePants = new Product('blue pants', 10, 'XL');
+(() => {
+    const bluePants = new Product('Blue large pants', 10, 'M');
     console.log(bluePants.toString())
 })();
