3c2f30dc04d726ae46fdbd3bfd5efb60e08342f1
Camila Serantoni <camila.serantoni@blue.cl>
2024-10-08 09:01:35 -0300

    inicialización para entender principio de responsabilidad unica

---
+++ src/clean-code/06-classes-a.ts
@@ -0,0 +1,23 @@
+(()=>{
+
+
+    type Gender = 'M'|'F';
+class Person {
+
+
+    constructor( 
+        public name: string,
+        public gender: Gender, 
+        public birthdate: Date
+    ){
+       
+    }
+
+
+}
+
+    const newPerson = new Person('Pepito', 'M', new Date('1985-10-21'));
+    console.log({newPerson})
+
+
+})()
