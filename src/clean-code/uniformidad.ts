e30655f9916e2126334abf1d17394fff41b66263
Camila Serantoni <camila.serantoni@blue.cl>
2024-10-09 07:57:25 -0300

    uniformidad y comentarios

---
+++ src/clean-code/uniformidad.ts
@@ -0,0 +1,14 @@
+/*
+Uniformidad del proyecto
+- problemas similares, soluciones similares
+
+const createProduct/ updateProduct 
+updateProduct /updateUser  
+
+- no solo a los nombres de las funciones, sino que al
+
+- Indentación:  es importante pero a discreción del usuario 
+pero es bueno que tenga uniformidad dentro del proyecto
+
+
+*/
