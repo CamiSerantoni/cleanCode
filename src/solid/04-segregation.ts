05d9a85dac811481bff84aff79f58179851541cc
Camila Serantoni <camila.serantoni@blue.cl>
2024-10-10 12:01:57 -0300

    estableciendo ejercicio inyeccion de dependencias

--- src/solid/04-segregation.ts
+++ src/solid/04-segregation.ts
@@ -41,3 +41,28 @@ class Penguin implements Bird, SwimmerBird{
     public eat(){ }
     public swim(){ }
 }
+
+
+/*
+
+Principio de Segregación de interface: 
+
+Si hacemos algún tipo de cambio en alguna interface 
+solo afecta a las clases que tocan a esa interfaz 
+no deben ser obligados por implementación o por padre 
+a usar propiedades o métodos  que no neceseitan o no usan
+
+por ejemplo en el caso de volar de la avestruz 
+esto hara mi codigo vulnerable al cambio 
+
+*/
+
+
+/*
+Cómo detectar violaciones a ISP ? 
+
+Si las interfaces que diseñamos nos obligan 
+a violar principios de responsabilidad unica
+y sustitución de Liskov
+
+*/
